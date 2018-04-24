03e2dd8e6f7712abcbdfe5e79d3809847ba1b5fd
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-04-24 11:47:57 +0200

    Update project

--- src/sleepuntil.go
+++ src/sleepuntil.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 const (
@@ -27,7 +29,7 @@ func init() {
 	globalFlagSet.BoolVar(&globalFlags.Animate, "animate", false, "Show an animation for the remaining time")
 }
 
-func showHelp() {
+func showHelpAndExit() {
 	fmt.Printf("Usage: %s <time> <flags>\n\n", cliName)
 
 	myFlags := make([]*flag.Flag, 0)
@@ -44,6 +46,22 @@ func showHelp() {
 	fmt.Printf("Version: %g\n", cliVersion)
 	fmt.Printf("Please report bugs here: https://github.com/jnidzwetzki/sleepuntil/issues\n")
 	fmt.Printf("\n")
+
+	os.Exit(-1)
+}
+
+func parseDate(dateToParse string) (*time.Time, error) {
+	dateLayouts := []string{"15:04", "15:04:05", "2006-01-02T15:04:05.000Z"}
+
+	for _, layout := range dateLayouts {
+		t, err := time.Parse(layout, dateToParse)
+
+		if err == nil {
+			return &t, nil
+		}
+	}
+
+	return nil, errors.New("Unable to parse date: " + dateToParse)
 }
 
 func main() {
@@ -51,6 +69,15 @@ func main() {
 	var args = globalFlagSet.Args()
 
 	if len(args) < 1 {
-		showHelp()
+		showHelpAndExit()
 	}
+
+	parseDate, err := parseDate(args[0])
+
+	if err != nil {
+		fmt.Printf("Unable to parse %s, exiting\n", args[0])
+		os.Exit(-1)
+	}
+
+	fmt.Printf("Wait until %s\n", *parseDate)
 }
