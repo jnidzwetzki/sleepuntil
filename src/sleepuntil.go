271d3917f8e3b5cac5bd4a3c59152633a308096f
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-04-24 08:41:36 +0200

    Update

--- src/sleepuntil.go
+++ src/sleepuntil.go
@@ -4,17 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"text/tabwriter"
 )
 
 const (
+	cliVersion    = 0.1
 	cliName       = "sleepuntil"
 	cliDesription = "Sleep until a certain time is reached"
 )
 
 var (
-	out *tabwriter.Writer
-
 	globalFlagSet = flag.NewFlagSet("sleepuntil", flag.ExitOnError)
 
 	globalFlags = struct {
@@ -25,23 +23,27 @@ var (
 )
 
 func init() {
-	out = new(tabwriter.Writer)
-	out.Init(os.Stdout, 0, 8, 1, '\t', 0)
 	globalFlagSet.BoolVar(&globalFlags.Verbose, "verbose", false, "Be verbose.")
 	globalFlagSet.BoolVar(&globalFlags.Animate, "animate", false, "Show an animation for the remaining time")
 }
 
 func showHelp() {
-	fmt.Printf("Usage: %s <time> <parameter>\n", cliName)
+	fmt.Printf("Usage: %s <time> <flags>\n\n", cliName)
 
 	myFlags := make([]*flag.Flag, 0)
 	globalFlagSet.VisitAll(func(f *flag.Flag) {
 		myFlags = append(myFlags, f)
 	})
 
+	fmt.Printf("Supported flags:\n")
 	for _, flag := range myFlags {
-		fmt.Printf("Flag: -%s (Default: %s) %s\n", flag.Name, flag.Value, flag.Usage)
+		fmt.Printf("-%s (Default: %s) %s\n", flag.Name, flag.Value, flag.Usage)
 	}
+
+	fmt.Printf("\n")
+	fmt.Printf("Version: %g\n", cliVersion)
+	fmt.Printf("Please report bugs here: https://github.com/jnidzwetzki/sleepuntil/issues\n")
+	fmt.Printf("\n")
 }
 
 func main() {
