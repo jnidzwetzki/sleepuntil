da1643df9e2dd3cdc715b21f82a2659ab88aba2a
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-04-27 08:11:22 +0200

    Update date parsing

--- src/sleepuntil.go
+++ src/sleepuntil.go
@@ -71,16 +71,37 @@ func parseDate(userTimeValue string) (*time.Time, error) {
 		return parsedValue, nil
 	}
 
+	// Try parsing with date string
 	var currentTime = time.Now().Local()
 	var datePrefix = currentTime.Format("2006-01-02")
 
 	var userTimeValueWithPrefix = datePrefix + " " + userTimeValue
 
-	return tryDateFormats(userTimeValueWithPrefix, layouts)
+	parsedValue, err = tryDateFormats(userTimeValueWithPrefix, layouts)
+
+	// Unable to parse, exit
+	if err != nil {
+		return nil, err
+	}
+
+	// Got hour value in the past (assume tomorrow is meant)
+	if parsedValue.Before(currentTime) {
+		var tomorrow = currentTime.AddDate(0, 0, 1)
+		var datePrefix = tomorrow.Format("2006-01-02")
+		var userTimeValueWithPrefixTomorrow = datePrefix + " " + userTimeValue
+		return tryDateFormats(userTimeValueWithPrefixTomorrow, layouts)
+	}
+
+	return parsedValue, nil
 }
 
 func main() {
-	globalFlagSet.Parse(os.Args[1:])
+	flagErr := globalFlagSet.Parse(os.Args[1:])
+
+	if flagErr != nil {
+		showHelpAndExit()
+	}
+
 	var args = globalFlagSet.Args()
 
 	if len(args) < 1 {
