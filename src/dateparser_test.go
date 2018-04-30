c262898ce1a2bf9fef753f507f5239c08661f9fc
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2018-04-30 14:37:19 +0200

    Added unit test

--- src/dateparser_test.go
+++ src/dateparser_test.go
@@ -3,5 +3,18 @@ package main
 import "testing"
 
 func testDateParsin(t *testing.T) {
+	date1 := "12:00:12"
+	date2 := "2006-01-02 15:04:05"
+	date3 := "2006-01-02 15:04:05"
+
+	dates := []string{date1, date2, date3}
+
+	for _, v := range dates {
+		_, err := parseDate(v)
+
+		if err != nil {
+			t.Errorf("Got error %s", err)
+		}
+	}
 
 }
